55aba17368c8288866b5af67f29ee87d8d2d6cf1
s-k-zaman <sudo.coder.zaman@gmail.com>
2025-02-24 15:45:24 +0530

    fix: template box in manual report

--- frontend_rac/src/lib/analysis/templates/analysis-templates.tsx
+++ frontend_rac/src/lib/analysis/templates/analysis-templates.tsx
@@ -1,18 +1,19 @@
 import { InterviewAnalysis } from "@/lib/types";
 
+//INFO: if needed blank boxes use different object keys [no of spaces]
 export const base: InterviewAnalysis = {
   name: "",
   date: new Date().toISOString(),
   strengths: {
     "": "",
-    "": "",
-    "": "",
+    " ": "",
+    "  ": "",
   },
   areas_to_target: {
     "": "",
-    "": "",
-    "": "",
-    "": "",
+    " ": "",
+    "  ": "",
+    "   ": "",
   },
   next_steps: [
     { main: "", sub_points: ["", "", ""] },
