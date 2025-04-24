e8f0b1be423479e3465ae6fbc58e85a8b23542b3
s-k-zaman <sudo.coder.zaman@gmail.com>
2025-04-24 22:40:22 +0530

    update: sidebar

--- frontend_rac/src/components/sidebar/AppSidebar.tsx
+++ frontend_rac/src/components/sidebar/AppSidebar.tsx
@@ -8,9 +8,13 @@ import { TaskHistory } from "./tasks/TaskHistory";
 
 export function AppSidebar() {
   return (
-    <div className="">
+    <div className="max-w-[240px]">
       <div>
-        <Sidebar variant="inset" className="z-50 ">
+        <Sidebar
+          variant="inset"
+          className="z-50 shadow-md border-r border-gray-200"
+          style={{ "--sidebar-width": "240px" } as React.CSSProperties}
+        >
           <div className="absolute top-4 -right-10">
             <SidebarTrigger />
           </div>
